8e8b44acd5808cc8e18646cd3dfc368affa97849
sai.meda1 <saivmeda@gmail.com>
2024-02-25 14:11:46 -0500

    added templates

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// script.js
 
+document.addEventListener('DOMContentLoaded', function() {
+  
+    const toggleButton = document.querySelector('.toggle-button');
+    const navbarLinks = document.querySelector('.navbar-links');
+  
+    toggleButton.addEventListener('click', () => {
+      navbarLinks.classList.toggle('active');
+    });
+  
+  });
+  
